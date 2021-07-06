c49990c904ac73df5d35b3c3cad6596720a5f59f
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-07-06 22:25:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-song="";
+song1="";
+volume=1;
 function preload(){
-    song=loadSound("Music.mp3");
+    song1=loadSound("Song.mp3");
 }
 function setup(){
     video=createCapture(VIDEO);
     video.size(500,500);
     video.parent('liveVideo');
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', Results);
+}
+function modelLoaded(){
+    console.log("The model is loaded");
+}
+function Results(results){
+    if(results.length>0){
+        if(results[0].pose.keypoints[10].score>0.2){
+            volume=Number(results[0].pose.rightWrist.y)/500;
+            document.getElementById('volume').textContent=volume;
+        }        
+    }
 }
 function Play(){
-    console.log(song);
-    getAudioContext().resume();
-    song.play();
+    song1.play();
+    song1.setVolume(volume);
 }
